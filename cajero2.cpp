abaf33e893db8cbb3a892dd70baa284d35b8df7b
Adrianguanoluisaloza <145121214+Adrianguanoluisaloza@users.noreply.github.com>
2023-10-22 23:16:59 -0500

    Update and rename cajero3..cpp to cajero2.cpp

---
+++ cajero2.cpp
@@ -0,0 +1,20 @@
+//programa: clave
+//autor: adrian guanoluisa
+//fecha: 2/10/2023
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+        int c1,c2=5;
+        cout<<"Ingrese la  clave:";cin>>c1;
+        if(c1==c2)
+        {
+                cout<<"Acceso correcto";
+        }else{
+                cout<<"Acceso incorrecto";
+        }
+        return(0);
+}
+}
